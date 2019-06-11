5f78fa1afd2c51c69854edec6c76101fe22dce7b
James Cox <jamesncox@yahoo.com>
2019-06-11 21:02:23 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -34,7 +34,11 @@ class App < Sinatra::Base
       when "add"
         answer = (number1 + number2).to_s 
       when "subtract"
-        answer = (number)
+        answer = (number1 - number2).to_s 
+      when "multiply"
+        answer = (number1 * number2).to_s 
+      when "divide"
+        answer = (number1 / number2).to_s 
     end 
   end 
 end
