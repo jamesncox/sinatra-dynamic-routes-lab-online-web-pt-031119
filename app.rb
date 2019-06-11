d3b85da14f277e9939d85dfb24d363782ce191b8
James Cox <jamesncox@yahoo.com>
2019-06-11 20:44:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,7 +4,8 @@ class App < Sinatra::Base
   # Write your code here!
 
   get '/reversename/:name' do
-    
+    @reversename = params[:name].reverse 
+    @reversename
   end 
   
   get '/square/:number' do 
