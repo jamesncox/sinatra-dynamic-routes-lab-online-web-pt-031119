327acbae14fbc956f7d5cc51e2cafe94c4e5bba4
James Cox <jamesncox@yahoo.com>
2019-06-11 21:00:03 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -28,7 +28,9 @@ class App < Sinatra::Base
     number1 = params[:number1].to_i 
     number2 = params[:number2].to_i 
     
-    answer = "unable to perform this operatoin"
+    answer = "Unable to perform this operation"
+    
+    case 
     
   end 
 end
